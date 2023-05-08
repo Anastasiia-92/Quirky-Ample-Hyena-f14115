099c2500ed6cdbb8159ff9b4eae3b625760e2331
Anastasiia-92 <73040003+Anastasiia-92@users.noreply.github.com>
2023-05-08 13:53:59 +0300

    Commit from TeleportHQ

---
+++ src/teleporthq/components/super-banner.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import './super-banner.css'
+
+const SuperBanner = (props) => {
+  return (
+    <div className="super-banner-container">
+      <div className="super-banner-frame">
+        <div className="super-banner-frame544">
+          <span className="super-banner-text">
+            <span>Соединенное Королевство Великобритании</span>
+          </span>
+        </div>
+        <img
+          src="/playground_assets/Rectangle19-oekh.png"
+          alt="Rectangle19"
+          className="super-banner-rectangle"
+        />
+        <div className="super-banner-frame545">
+          <span className="super-banner-text2">
+            <span>Украина</span>
+          </span>
+        </div>
+        <img
+          src="/playground_assets/Rectangle112-v6w.png"
+          alt="Rectangle112"
+          className="super-banner-rectangle1"
+        />
+        <div className="super-banner-frame546">
+          <span className="super-banner-text4">
+            <span>Босния и Герцеговина</span>
+          </span>
+        </div>
+        <img
+          src="/playground_assets/Rectangle115-u57r.png"
+          alt="Rectangle115"
+          className="super-banner-rectangle2"
+        />
+        <div className="super-banner-frame547">
+          <span className="super-banner-text6">
+            <span>Китай</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SuperBanner
